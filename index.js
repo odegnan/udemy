1fb166c1da425cfaa83d1a8d9ea0406a027666e5
Oliver Degnan <oliver@Olivers-MacBook-Pro.local>
2018-04-02 11:18:40 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there Oliver!' });
+  res.send({ hi: 'there buddy!' });
 });
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
+
+//HEROKU app
+//https://agile-mesa-61866.herokuapp.com/ | https://git.heroku.com/agile-mesa-61866.git
