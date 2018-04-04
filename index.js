9b8af5c01284704e44e0e84173b0f8cb963a2a7c
Oliver Degnan <oliver@Olivers-MacBook-Pro.local>
2018-04-04 08:21:15 -0500

    Aithentication for Google completed

--- index.js
+++ index.js
@@ -2,9 +2,25 @@
 //https://agile-mesa-61866.herokuapp.com/ | https://git.heroku.com/agile-mesa-61866.git
 
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
